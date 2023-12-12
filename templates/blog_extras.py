4012073a9f513e3abb9876da06cfbf0325990e2f
Akmaljon Yoqubov <akmaljonyoqubov088@gmail.com>
2023-12-12 11:25:50 +0000

    Finish custom template tags

--- templates/blog_extras.py
+++ templates/blog_extras.py
@@ -1,5 +1,6 @@
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from blog.models import Post
 
 
 @register.filter
@@ -22,3 +23,34 @@ def author_details(author):
         suffix = ""
 
     return mark_safe(f"{prefix}{name}{suffix}")
+
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
